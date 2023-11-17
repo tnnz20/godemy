143bf416fed56654866b82562e3271d506ecc2f7
tnnz20 <kaitora200@gmail.com>
2023-11-17 23:25:27 +0800

    feat: refactor flatten function and change to button

--- src/components/pager.tsx
+++ src/components/pager.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
+import { SidebarNavItem } from "@/types"
 import { Chapter } from "contentlayer/generated"
 
 import { chaptersConfig } from "@/config/chapters"
-import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 
-import { buttonVariants } from "./ui/button"
+import { Button } from "./ui/button"
 
 interface PagerProps {
   chapter: Chapter
@@ -18,24 +18,28 @@ export function Pager({ chapter }: Readonly<PagerProps>) {
   return (
     <div className="flex flex-row items-center justify-between">
       {pager?.prev && (
-        <Link href={pager.prev.path} className={cn(buttonVariants({ variant: "ghost" }))}>
-          <Icons.ChevronLeft className="mr-2 h-4 w-4" />
-          {pager.prev.title}
-        </Link>
+        <Button variant={"ghost"} asChild>
+          <Link href={pager.prev.href} className="flex flex-row items-center">
+            <Icons.ChevronLeft className="mr-2 h-4 w-4" />
+            {pager.prev.title}
+          </Link>
+        </Button>
       )}
       {pager?.next && (
-        <Link href={pager.next.path} className={cn(buttonVariants({ variant: "ghost" }), "ml-auto")}>
-          {pager.next.title}
-          <Icons.ChevronRight className="ml-2 h-4 w-4" />
-        </Link>
+        <Button variant={"ghost"} className="ml-auto" asChild>
+          <Link href={pager.next.href} className="flex flex-row items-center">
+            {pager.next.title}
+            <Icons.ChevronRight className="ml-2 h-4 w-4" />
+          </Link>
+        </Button>
       )}
     </div>
   )
 }
 
 export function getPagerForDoc(chapter: Chapter) {
-  const flattenedLinks = [null, ...flatten({ links: chaptersConfig.sideNav }), null]
-  const activeIndex = flattenedLinks.findIndex((link) => chapter.slug === link?.path)
+  const flattenedLinks = [null, ...flatten(chaptersConfig.sideNav), null]
+  const activeIndex = flattenedLinks.findIndex((link) => chapter.slug === link?.href)
   const prev = activeIndex !== 0 ? flattenedLinks[activeIndex - 1] : null
   const next = activeIndex !== flattenedLinks.length - 1 ? flattenedLinks[activeIndex + 1] : null
 
@@ -44,9 +48,6 @@ export function getPagerForDoc(chapter: Chapter) {
     next,
   }
 }
-
-export function flatten({ links }: { links: { items?: {}[] }[] }): any {
-  return links.reduce((flat, link) => {
-    return flat.concat(link.items ? flatten({ links: link.items }) : link)
-  }, [])
+export function flatten(links: SidebarNavItem[]) {
+  return links.map((link) => link.items?.map((item) => item)).flat(2)
 }
