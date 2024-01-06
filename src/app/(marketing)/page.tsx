6f03db83ba669f3fce926f78d05c23832009cdfe
tnnz20 <kaitora200@gmail.com>
2024-01-06 19:30:01 +0800

    refactor: marketing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <section className="container flex min-h-screen flex-col items-center justify-center gap-4">
         <h1 className="mx-auto flex max-w-sm flex-col items-center justify-center  gap-2 text-center font-heading text-2xl font-medium md:max-w-md md:text-3xl lg:max-w-fit lg:flex-row lg:gap-1 lg:text-4xl">
-          <span className="mb-2 border-2 border-slate-600 bg-accent p-1 font-logo tracking-wide shadow-sm">Godemy</span>{" "}
+          <span className="mb-2 mr-4 border-2 border-slate-600 bg-accent p-1 font-logo tracking-wide shadow-sm">
+            Godemy
+          </span>{" "}
           Tingkatkan Kode Anda, Penguasaan Golang dengan Panduan Ahli
         </h1>
         <p className="max-w-[32rem] text-center text-base text-muted-foreground md:max-w-3xl md:text-xl lg:max-w-4xl ">
@@ -22,11 +24,11 @@ export default function Home() {
           <Link href="/">Mulai Sekarang</Link>
         </Button>
       </section>
-      <section id="panduan" className="container h-full">
+      {/* <section id="panduan" className="container h-full">
         <div className="flex flex-col items-center justify-center">
           <h2 className="font-heading text-xl">Panduan</h2>
         </div>
-      </section>
+      </section> */}
     </div>
   )
 }
