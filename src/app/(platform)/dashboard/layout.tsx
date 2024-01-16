dc74bbfd0743e629be38cd7482589159f694ba00
tnnz20 <kaitora200@gmail.com>
2024-01-16 12:23:17 +0800

    refactor: naming component

---
+++ src/app/(platform)/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import SideNav from "./_components/sidenav"
+
+interface DocsLayoutProps {
+  children: React.ReactNode
+}
+
+export default function DocsLayout({ children }: Readonly<DocsLayoutProps>) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  )
+}
