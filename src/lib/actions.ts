9c4213035880b290a60c04300072d908a8dc4feb
tnnz20 <kaitora200@gmail.com>
2024-01-21 12:38:12 +0800

    feat: add registration action and fix error when failed fetch

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 import { LoginSchema, RegisterSchema } from "@/validators/auth"
 
-import { LoginState } from "@/types/auth"
+import { LoginState, RegisterState } from "@/types/auth"
 
 export async function signIn(prevState: LoginState, formData: FormData) {
   const validateFields = LoginSchema.safeParse({
@@ -13,7 +13,6 @@ export async function signIn(prevState: LoginState, formData: FormData) {
     password: formData.get("password"),
   })
 
-  console.log()
   if (!validateFields.success) {
     return {
       errors: validateFields.error.flatten().fieldErrors,
@@ -57,8 +56,67 @@ export async function signIn(prevState: LoginState, formData: FormData) {
     }
   } catch (error) {
     console.error(error)
+    return { message: `${error}` }
   }
 
   revalidatePath("/dashboard")
   redirect("/dashboard")
+}
+
+export async function signUp(prevState: RegisterState, formData: FormData) {
+  const validateFields = RegisterSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    gender: formData.get("gender"),
+    role: formData.get("role"),
+  })
+
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: "Validations",
+    }
+  }
+
+  const { name, email, password, gender, role } = validateFields.data
+
+  try {
+    const response = await fetch(`${process.env.BASE_DIR}/user/sign-up`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password, gender, role }),
+      referrer: `${process.env.BASE_DIR}`,
+    })
+
+    const data = await response.json()
+    console.log(response)
+    if (response.ok) {
+      const token = data.data.access_token
+      cookies().set({
+        name: "token",
+        secure: true,
+        value: token,
+        httpOnly: true,
+        path: "/",
+        maxAge: 60 * 60,
+      })
+    } else if (response.status == 409) {
+      return {
+        errors: {},
+        message: "Already",
+      }
+    } else {
+      return {
+        errors: {},
+        message: "Failed",
+      }
+    }
+  } catch (error) {
+    return { message: `${error}` }
+  }
+  revalidatePath("/login")
+  redirect("/login")
 }
