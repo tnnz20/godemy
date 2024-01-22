3fe7562bfe3b44aba1f5bb1480e5cd913d15c50f
tnnz20 <kaitora200@gmail.com>
2024-01-22 12:19:03 +0800

    feat: update type for dashboard

--- src/types/dashboard.d.ts
+++ src/types/dashboard.d.ts
@@ -3,3 +3,20 @@ export type SidebarNavItem = {
   href: string
   icon
 }
+
+export type CardWrapperProps = {
+  USER_TOKEN: string
+}
+
+export type ProfileProps = {
+  USER_TOKEN: string
+}
+
+export type ProfilesData = {
+  title: string
+  value: string
+}
+
+export type CardProfileProps = {
+  Profiles: ProfilesData[]
+}
